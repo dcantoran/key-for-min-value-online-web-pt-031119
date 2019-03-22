b4022c6eda04441959c3d1a6d532b36bf7d67925
David Cantoran <davidcantoran36@gmail.com>
2019-03-22 00:53:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,6 @@ def key_for_min_value(name_hash)
     if smallest_value == 0 || value < smallest_value
       smallest_value = value 
       smallest_key = key
-      
     end 
   end 
   smallest_key
