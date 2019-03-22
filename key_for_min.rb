31e991c507a2472a3b05ab4f83d2d219a4df0aaa
David Cantoran <davidcantoran36@gmail.com>
2019-03-22 00:35:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
     nil 
   else
     smallest_key = name_hash[0]
-  smallest_value = name_hash[1]
+    smallest_value = name_hash[1]
     name_hash.each do |item, value|
       if value < smallest_value
         smallest_value = value
